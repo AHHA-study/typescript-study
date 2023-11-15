62656588f9b3d85ddd5cd23a3586ddb7a38ce6b0
SangHyun <duddjshfl@naver.com>
2023-11-15 23:20:12 +0900

    fix: 11/15 update

--- src/app/study/1/sanghyun/component/table.tsx
+++ src/app/study/1/sanghyun/component/table.tsx
@@ -1,16 +1,37 @@
 import React, { ReactNode } from 'react'
 
+import './table.css'
+
+interface Data {
+  id: number
+  name: string
+  value: string
+}
+
 interface TableProps {
-  data: string[];
+  data: Data[]
 }
 
 const Table = (props: TableProps): ReactNode => {
   const { data } = props
 
   return (
-    <>
-      {JSON.stringify(data)}
-    </>
+    <div className='table'>
+      <header className='table__header'>
+        <div className='table__header--id'>id</div>
+        <div className='table__header--name'>name</div>
+        <div className='table__header--value'>value</div>
+      </header>
+      <ul>
+        {data.map(({id, name, value}) => (
+          <div className='table__row'>
+            <li className='table__row--id'>{id}</li>
+            <li className='table__row--name'>{name}</li>
+            <li className='table__row--value'>{value}</li>
+          </div>
+        ))}
+      </ul>
+    </div>
   )
 }
 
