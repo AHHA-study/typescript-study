e46301abafec1223fde5de8307aca677eedc9fd0
Yoon Gwan <blackgar@naver.com>
2023-11-20 23:44:52 +0900

    1번 문항 풀이

--- src/app/study/1/gwan/page.tsx
+++ src/app/study/1/gwan/page.tsx
@@ -1,5 +1,55 @@
+"use client"
+
+import { useEffect, useState } from "react";
+
+interface DataType {
+  id: 'number';
+  name: 'string';
+  value: 'string';
+}
+
 const Main = () => {
-  return <div>main</div>
+  const [listData, setListData] = useState<DataType[]>([])
+  const [startIndex, setStartIndex] = useState(0)
+
+  const getData = async () => {
+    const res = await fetch(`http://localhost:3000/api/random?size=10&index=${startIndex}`);
+    if (!res.ok) {
+      throw new Error('data fetch error.');
+    }
+    console.log('fetching url:', `http://localhost:3000/api/random?size=10&index=${startIndex}`)
+    return res.json();
+  };
+
+  const fetchData = async () => {
+    const { data } = await getData()
+    const newData = listData.concat(data)
+    console.log('startIndex:', startIndex)
+    console.log('newData:', newData)
+    setListData(newData)
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [startIndex])
+
+  console.log('listData:', listData)
+  console.log('startIndex:', startIndex)
+  return <div>
+    {listData.map(({id, name, value}) => (
+      <div key={id}>
+        <div>{id}</div>
+        <div>{name}</div>
+        <div>{value}</div>
+      </div>))}
+    
+    <button
+      style={{ padding: '1rem', marginTop: '1rem', border: '2px solid white' }}
+      onClick={() => setStartIndex((prev) => prev + 10)}
+    >
+      더보기
+    </button>
+  </div>
 }
 
 export default Main
